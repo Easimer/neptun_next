d7a501dbb4c8fc3e60e055ebc2f22179b40f88d0
Daniel Meszaros <easimer@gmail.com>
2019-09-04 17:00:20 +0200

    Implement short/verbose mode, simplify code
    - Add verbose mode
    - The default mode is now 'verbose'
    - Add '-s' cmdline flag
    - Delegate the responsibility of opening an ICS file from get_next_event
    to it's caller
    - Add helper function open_ics_cfg_url() to read the 'ics_url'
    configuration entry and open an ICS file in one call.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,21 @@
 #include <stddef.h>
 #include <string.h>
 #include <time.h>
+#include <assert.h>
 #include "platform.h"
 #include "ics.h"
 #include "ini_config.h"
 
+struct cmdargs_t {
+    bool status_bar = false; // -s
+};
+
 static ini_config cfg = NULL;
 
+//
+// Function: load_cfg
+// Load the configuration file
+//
 static void load_cfg() {
     char* path = NULL;
     int path_len = 0;
@@ -22,13 +31,70 @@ static void load_cfg() {
     free(path);
 }
 
+//
+// Function: unload_cfg
+// Unload the configuration file
+//
 static void unload_cfg() {
     if(cfg) {
         close_ini_config(cfg);
     }
 }
 
-static void print_event(const ics_event* ev, bool ongoing) {
+//
+// Function: open_ics_cfg_url
+// Read the ICS URL from the config file
+// and open it.
+//
+// Returns: the ICS handle
+[[nodiscard]]
+static ics open_ics_cfg_url() {
+    ics ret = NULL;
+    const char* url = NULL;
+
+    if(cfg) {
+        if(get_ini_config_str(cfg, "ics_url", &url)) {
+            ret = open_ics(url);
+        }
+    }
+
+    return ret;
+}
+
+//
+// Function: is_same_day
+// Determines whether two `struct tm`s are describing
+// a point in time on the same day.
+//
+// lhs: a struct tm
+// rhs: a struct tm
+//
+// Returns: the result
+[[nodiscard]]
+static bool is_same_day(const struct tm* lhs, const struct tm* rhs) {
+    bool ret = true;
+
+    if(lhs->tm_year != rhs->tm_year) {
+        ret = false;
+    }
+    if(lhs->tm_mon != rhs->tm_mon) {
+        ret = false;
+    }
+    if(lhs->tm_mday != rhs->tm_mday) {
+        ret = false;
+    }
+
+    return ret;
+}
+
+//
+// Function: print_event_short
+// Print the ICS event in the short format
+// Example: "⏩09:30AM Analízis I. (ABCDEF...) @ A.105"
+//
+// ev: pointer to the ICS event
+// ongoing: is the event an ongoing event
+static void print_event_short(const ics_event* ev, bool ongoing) {
     char timebuf[64];
     char truncbuf[32];
     struct tm tp;
@@ -50,58 +116,147 @@ static void print_event(const ics_event* ev, bool ongoing) {
     fprintf(stdout, "%s%s %s @ %s\n", prefix, timebuf, truncbuf, ev->location);
 }
 
-int main(int argc, char** argv) {
-    ics ics_inst;
-    const char* ics_url;
+//
+// Function: print_event_verbose
+// Print an event in the long format
+// Example:
+// ```
+// NAME
+// Location: LOCATION
+// Starts at: DATE TIME // not present if ongoing
+// Ends at: [DATE] TIME // DATE not present if start and end time is on the same day
+// ```
+//
+// ev: pointer to the event
+// ongoing: is the event an ongoing event
+static void print_event_verbose(const ics_event* ev, bool ongoing) {
+    char buf[64];
+    struct tm tmstart, tmend;
+    time_t tstart, tend;
+
+    tstart = (time_t)ev->date_start;
+    tend = (time_t)ev->date_end;
+    localtime_r(&tstart, &tmstart);
+    localtime_r(&tend, &tmend);
+
+    fprintf(stdout, "%s\n", ev->name);
+    fprintf(stdout, "Location: %s\n", ev->location);
+    if(!ongoing) {
+        strftime(buf, 64, "%F %I:%M%p", &tmstart);
+        fprintf(stdout, "Starts at: %s\n", buf);
+    }
+    if(is_same_day(&tmstart, &tmend)) {
+        strftime(buf, 64, "%I:%M%p", &tmend);
+        fprintf(stdout, "Ends at: %s\n", buf);
+    } else {
+        strftime(buf, 64, "%F %I:%M%p", &tmend);
+        fprintf(stdout, "Ends at: %s\n", buf);
+    }
+}
+
+// 
+// Function: get_next_event
+// Retrieves the next future event in an ICS file
+// 
+// ev_buf: where the details of the event will be written (REQUIRED)
+// ongoing: whether the event is ongoing or not (REQUIRED)
+// ics_inst: handle to ICS file (REQUIRED)
+//
+// Returns: value of ev_buf if such an event was found or NULL
+[[nodiscard]]
+static ics_event* get_next_event(ics_event* ev_buf, bool* ongoing, ics ics_inst) {
+    ics_event* ret = NULL;
     ics_event ev, ev_next;
-    load_cfg();
     u64 curtime = time(NULL);
     u64 time_next = (u64)-1;
-    bool ongoing = false;
     bool found_event = false;
 
-    ev_next.name = ev_next.location = NULL;
+    assert(cfg);
+    assert(ics_inst);
+    assert(ongoing);
 
-    if(cfg) {
-        if(get_ini_config_str(cfg, "ics_url", &ics_url)) {
-            ics_inst = open_ics(ics_url);
-            if(ics_inst) {
-                while(next_ics_event(ics_inst, &ev)) {
-                    if(ev.date_end > curtime) {
-                        if(ev.date_start <= curtime) {
-                            // event is ongoing
-                            ev_next = ev;
-                            curtime = ev.date_start;
-                            ongoing = true;
-                            found_event = true;
-                        } else {
-                            // event is coming up
-                            if(ev.date_start < time_next) {
-                                time_next = ev.date_start;
-                                ev_next = ev;
-                                found_event = true;
-                            }
-                        }
-                    }
-                    if(ev.date_end >= curtime && ev.date_start < ev_next.date_start) {
-                        ev_next = ev;
-                        found_event = true;
-                    }
-                }
-                if(found_event) {
-                    print_event(&ev_next, ongoing);
-                }
-                close_ics(ics_inst);
+    while(next_ics_event(ics_inst, &ev)) {
+        if(ev.date_end > curtime) {
+            if(ev.date_start <= curtime) {
+                // event is ongoing
+                ev_next = ev;
+                curtime = ev.date_start;
+                *ongoing = true;
+                found_event = true;
             } else {
-                fprintf(stderr, "Failed to open ICS file\n");
+                // event is coming up
+                if(ev.date_start < time_next) {
+                    time_next = ev.date_start;
+                    ev_next = ev;
+                    found_event = true;
+                }
             }
-        } else {
-            fprintf(stderr, "No ics_url present in config file! (~/.config/neptun_next.cfg)\n");
         }
-        unload_cfg();
-    } else {
-        fprintf(stderr, "Failed to open config file\n");
+        if(ev.date_end >= curtime && ev.date_start < ev_next.date_start) {
+            ev_next = ev;
+            found_event = true;
+        }
     }
+    if(found_event) {
+        *ev_buf = ev_next;
+        ret = ev_buf;
+    }
+    return ret;
+}
+
+static void parse_cmdline(cmdargs_t* args, int argc, char** argv) {
+    for(int i = 1; i < argc; i++) {
+        if(argv[i][0] == '-') {
+            switch(argv[i][1]) {
+                case 's':
+                    args->status_bar = true;
+                    break;
+            }
+        }
+    }
+}
+
+static void status_bar(const cmdargs_t* args) {
+    ics ics_inst;
+    ics_event ev = {0};
+    bool ongoing = false;
+
+    ics_inst = open_ics_cfg_url();
+    if(ics_inst) {
+        if(get_next_event(&ev, &ongoing, ics_inst)) {
+            print_event_short(&ev, ongoing);
+        }
+        close_ics(ics_inst);
+    }
+}
+
+static void verbose_mode(const cmdargs_t* args) {
+    ics ics_inst;
+    ics_event ev = {0};
+    bool ongoing = false;
+
+    ics_inst = open_ics_cfg_url();
+    if(ics_inst) {
+        if(get_next_event(&ev, &ongoing, ics_inst)) {
+            print_event_verbose(&ev, ongoing);
+        }
+        close_ics(ics_inst);
+    }
+}
+
+int main(int argc, char** argv) {
+    cmdargs_t args;
+    load_cfg();
+
+    parse_cmdline(&args, argc, argv);
+
+    if(args.status_bar) {
+        status_bar(&args);
+    } else {
+        verbose_mode(&args);
+    }
+
+    unload_cfg();
 
     return 0;
 }
